00091afd1174dbb8829be1f85f777c213889c0af
moeby <tatyana.merlo@gmail.com>
2018-01-10 10:23:11 +0100

    charset corrected and tab title added

--- src/index.php
+++ src/index.php
@@ -1,41 +1,42 @@
 <?php
-	require_once("sessionCheck.php");
+require_once("sessionCheck.php");
 ?>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="resources/css/screen.css" />
+    <title>Schlosslauf</title>
+    <link rel="stylesheet" type="text/css" href="resources/css/screen.css"/>
+    <meta charset="UTF-8">
 </head>
 <body>
-	
-<div id="kopf"> 
-  <?PHP
-    include "view/kopf.php";
-  ?>
-</div>
-  
-<div id="inhalt_links">
+
+<div id="kopf">
     <?PHP
-	if(isLoggedIn()){
-		include "view/navigation.inc.php";
-	}
+    include "view/kopf.php";
     ?>
 </div>
-			
-			
-<div id="inhalt_mitte"> 
+
+<div id="inhalt_links">
     <?PHP
-	if(isLoggedIn()){
-		if(isset($_GET["inhalt_mitte"])){    
-			include($_GET["inhalt_mitte"]);
-		}
-		else{
-			include("view/home.html");
-		}
-	}else{
-		include("view/login_form.php");
-	}
-    ?>      
+    if (isLoggedIn()) {
+        include "view/navigation.inc.php";
+    }
+    ?>
 </div>
-        
+
+
+<div id="inhalt_mitte">
+    <?PHP
+    if (isLoggedIn()) {
+        if (isset($_GET["inhalt_mitte"])) {
+            include($_GET["inhalt_mitte"]);
+        } else {
+            include("view/home.html");
+        }
+    } else {
+        include("view/login_form.php");
+    }
+    ?>
+</div>
+
 </body>
 </html>
